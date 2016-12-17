16256a77a6b8af181687ac2d0f76941eebf8b173
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-17 10:32:45 +0000

    Show blank window

--- src/tree/Surface.java
+++ src/tree/Surface.java
@@ -1,9 +1,33 @@
 package tree;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
 import javax.swing.JPanel;
 
 public class Surface extends JPanel {
+  private final int WIDTH = 400;
+  private final int HEIGHT = 400;
   public Surface() {
+    setPreferredSize(new Dimension(WIDTH, HEIGHT));
+  }
 
+  public void doDrawing(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g.create();
+    g2d.setColor(Color.GRAY);
+    g2d.fillRect(0, 0, WIDTH, HEIGHT);
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    doDrawing(g);
+  }
+
+  public void render() {
+    paintImmediately(new Rectangle(0, 0, WIDTH, HEIGHT));
   }
 }
--- src/tree/Window.java
+++ src/tree/Window.java
@@ -4,6 +4,14 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
   public Window() {
+    setTitle("Fractal Trees");
+    setResizable(false);
 
+    Surface s = new Surface();
+    add(s);
+    pack();
+
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
   }
 }
