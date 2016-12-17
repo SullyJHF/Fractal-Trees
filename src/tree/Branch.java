50c376215d3f4889147a6f59a2b023d94e27b515
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-17 18:38:19 +0000

    initial drawing of a tree

--- src/tree/Branch.java
+++ src/tree/Branch.java
@@ -1,9 +1,59 @@
 package tree;
 
 import java.awt.Point;
+import java.awt.Shape;
+import java.awt.geom.Line2D;
+import java.util.Random;
 
 public class Branch {
-  public Branch(Point a, Point b) {
+  private float decreaseBy = 0.67f;
+  private float angleChange = 30;
+  private float angle;
+  private Random r;
+  private Point begin, end;
+  public boolean finished = false;
 
+  public Branch(Point begin, Point end) {
+    this.r = new Random();
+    this.begin = begin;
+    this.end = end;
+    this.angle = getAngle();
+  }
+
+  private float getAngle() {
+    System.out.println("(" + begin.x + ", " + begin.y + ") (" + end.x + ", " + end.y + ")");
+    float angle = (float) Math.toDegrees(Math.atan2(end.y - begin.y, end.x - begin.x));
+//    angle += 90;
+    if(angle < 0) angle += 360;
+    System.out.println(angle);
+    return angle;
+  }
+
+  public Branch(int x1, int y1, int x2, int y2) {
+    this(new Point(x1, y1), new Point(x2, y2));
+  }
+
+  public Branch branchRight() {
+    int x2 = this.end.x + (int) (Math.cos(Math.toRadians(angle + angleChange)) * 60.0);
+    int y2 = this.end.y + (int) (Math.sin(Math.toRadians(angle + angleChange)) * 60.0);
+    this.finished = true;
+    return new Branch(this.end.x, this.end.y, x2, y2);
+  }
+
+  public Branch branchLeft() {
+    int x2 = this.end.x + (int) (Math.cos(Math.toRadians(angle - angleChange)) * 60.0);
+    int y2 = this.end.y + (int) (Math.sin(Math.toRadians(angle - angleChange)) * 60.0);
+    this.finished = true;
+    return new Branch(this.end.x, this.end.y, x2, y2);
+  }
+
+  public Shape getLine() {
+    Line2D l = new Line2D.Double();
+    l.setLine(begin.x, begin.y, end.x, end.y);
+    return l;
+  }
+
+  public void jitter() {
+    end.x += r.nextInt(3) - 1;
   }
 }
--- src/tree/Surface.java
+++ src/tree/Surface.java
@@ -1,24 +1,48 @@
 package tree;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 public class Surface extends JPanel {
-  private final int WIDTH = 400;
-  private final int HEIGHT = 400;
+  private final int WIDTH = 800;
+  private final int HEIGHT = 800;
+
+  private ArrayList<Branch> tree;
+
   public Surface() {
     setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    tree = new ArrayList<Branch>();
+    Branch root = new Branch(WIDTH / 2, HEIGHT, WIDTH / 2, HEIGHT - 100);
+    tree.add(root);
+    tree.add(root.branchRight());
+    tree.add(root.branchLeft());
+    int count = 0;
+    for(int i = 0; i < 31; i++) {
+      if(tree.get(i).finished) continue;
+      tree.add(tree.get(i).branchRight());
+      tree.add(tree.get(i).branchLeft());
+    }
   }
 
   public void doDrawing(Graphics g) {
     Graphics2D g2d = (Graphics2D) g.create();
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
     g2d.setColor(Color.GRAY);
     g2d.fillRect(0, 0, WIDTH, HEIGHT);
+    g2d.setColor(Color.WHITE);
+    g2d.setStroke(new BasicStroke(5, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
+    for(Branch b : tree) {
+      g2d.draw(b.getLine());
+    }
   }
 
   @Override
